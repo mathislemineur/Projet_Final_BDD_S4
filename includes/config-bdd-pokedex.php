eca25177d8be8ade75053636925fd91d4a2af72a
KE9KE2 <ke9ke2@gmail.com>
2026-05-20 10:58:16 +0200

    test67

--- includes/config-bdd-pokedex.php
+++ includes/config-bdd-pokedex.php
@@ -3,7 +3,7 @@
 //Paramètres de connexion à la base de données
 define('SERVEUR', 'localhost');         //hote
 define('USER', 'root');                 //login, par exemple "root" ou ""
-define('PWD', 'root');                  //mot de passe, par exemple "root" ou ""
+define('PWD', 'Happy2006');                  //mot de passe, par exemple "root" ou ""
 define('DB_NAME', 'projet_final');               //nom de la base de données
 
 ?>
